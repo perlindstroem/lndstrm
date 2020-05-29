83b2a792f1608bb9598652ea7440fccdca565ba5
Per Lindström <per@digitaledge.se>
2020-05-29 15:44:53 +0200

    set up prismic content fetching

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,26 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+console.log('API KEY', process.env.API_KEY)
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `lndstrm`,
+        accessToken: `${process.env.API_KEY}`,
+        schemas: {
+          post: require('./src/schemas/post.json'),
+        },
+        // linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
+      },
+    },
+  ],
 }
+
+// module.exports = {}
