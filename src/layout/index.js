a77bd6040f962bc7f1589301e287fd54232f8abb
Per Lindström <per@digitaledge.se>
2020-06-01 19:46:18 +0200

    add favicons

--- src/layout/index.js
+++ src/layout/index.js
@@ -10,6 +10,24 @@ export default ({ children }) => {
       <Helmet defer={false}>
         <html lang="en" />
         <title>Per Lindström</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/static/site.webmanifest" />
       </Helmet>
       <Navigation />
       <div className="content">{children}</div>
