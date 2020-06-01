2a8ba561bcd651c98d55b84e1824cafb443ea983
Per Lindström <per@digitaledge.se>
2020-06-01 16:31:14 +0200

    hide prismic post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,31 +1,31 @@
-import React from "react"
-import { graphql } from "gatsby"
+// import React from "react"
+// import { graphql } from "gatsby"
 
-const Post = ({ data: { prismicPost } }) => {
-  const { data } = prismicPost
+// const Post = ({ data: { prismicPost } }) => {
+//   const { data } = prismicPost
   
-  return (
-    <React.Fragment>
-      <h1>{data.title.text}</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
-    </React.Fragment>
-  )
-}
+//   return (
+//     <React.Fragment>
+//       <h1>{data.title.text}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
+//     </React.Fragment>
+//   )
+// }
 
-export default Post
+// export default Post
 
-export const pageQuery = graphql`
-  query PostBySlug($uid: String!) {
-    prismicPost(uid: { eq: $uid }) {
-      uid
-      data {
-        title {
-          text
-        }
-        content {
-          html
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query PostBySlug($uid: String!) {
+//     prismicPost(uid: { eq: $uid }) {
+//       uid
+//       data {
+//         title {
+//           text
+//         }
+//         content {
+//           html
+//         }
+//       }
+//     }
+//   }
+// `
