f198427dee5b1c818d42f1d14dd70c954f205b10
Per Lindström <per@digitaledge.se>
2020-06-16 21:06:42 +0200

    add social links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,22 @@
 import React from "react"
 import Layout from "../layout"
+import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 export default () => {
   return (
     <Layout>
       <h1>Contact</h1>
       <p>
-        For any inquiries, please contact me on: <strong>per (at) lndstrm (dot) se</strong>.
+        For any inquiries, please contact me on:{" "}
+        <strong>per (at) lndstrm (dot) se</strong>.
       </p>
+      <p>I can also be found at any of the following:</p>
+      <a className="social" href="https://www.linkedin.com/in/lndstrm/">
+        <FaLinkedin />
+      </a>
+      <a className="social" href="https://github.com/perlindstroem">
+        <FaGithub />
+      </a>
     </Layout>
   )
 }
